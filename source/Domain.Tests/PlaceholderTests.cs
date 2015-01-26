de35efcaefaa5948121b3304a273c8697fe68e98
Mark Thiessen <mark.thiessen@devfacto.com>
2015-01-26 15:44:59 -0700

    Adds initial domain test project

---
+++ source/Domain.Tests/PlaceholderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace SiSystems.ClientApp.Domain.Tests
+{
+    [TestFixture]
+    public class PlaceholderTests
+    {
+        [Test]
+        public void PointlessTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
