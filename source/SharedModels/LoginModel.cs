6db334461d2718ba42b53519475bc0578c78ff1c
Mark Thiessen <mark.thiessen@devfacto.com>
2015-01-28 10:47:38 -0700

    Adds initial SharedModels project - intended for shared types between server & mobile apps

---
+++ source/SharedModels/LoginModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SiSystems.ClientApp.SharedModels
+{
+    public class LoginModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
