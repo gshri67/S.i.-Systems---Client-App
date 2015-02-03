8330681e8086df631d252345de5f30f69e28b4cf
Mark Thiessen <mark.thiessen@devfacto.com>
2015-02-03 10:07:59 -0700

    Adds simple test page for authenticate and call alumni API

--- source/Web/Controllers/HomeController.cs
+++ source/Web/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
 ﻿using System.Web.Mvc;
+using SiSystems.ClientApp.SharedModels;
 
 namespace SiSystems.ClientApp.Web.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
         {
             return View();
         }
