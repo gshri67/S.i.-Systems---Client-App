f4b1ad8bfe390ef5900ad9aa0edc1747c6daed0e
Mark Thiessen <mark.thiessen@devfacto.com>
2015-02-03 10:07:06 -0700

    Refactors and cleans up database usage

---
+++ source/Web.Domain/DatabaseContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SiSystems.ClientApp.Web.Domain
+{
+    public class DatabaseContext: IDisposable
+    {
+        private const string ConnectionStringConfigPropertyName = "ApplicationDb";
+
+        private IDbConnection _connection;
+        public IDbConnection Connection { get { return _connection ?? (_connection = CreateConnection()); } }
+
+        private IDbConnection CreateConnection()
+        {
+            return new SqlConnection(ConfigurationManager.ConnectionStrings[ConnectionStringConfigPropertyName].ConnectionString);
+        }
+
+        #region IDisposable Implementation
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                // free managed resources
+                if (_connection != null)
+                {
+                    _connection.Dispose();
+                    _connection = null;
+                }
+            }
+        }
+        #endregion
+    }
+}
--- source/Web.Domain/Repositories/UserRepository.cs
+++ source/Web.Domain/Repositories/UserRepository.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
+﻿using System.Linq;
 using Dapper;
 using SiSystems.ClientApp.SharedModels;
 
@@ -9,35 +6,25 @@ namespace SiSystems.ClientApp.Web.Domain.Repositories
 {
     public class UserRepository
     {
-        private IDbConnection CreateDbConnection()
-        {
-            var connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["ApplicationDb"].ConnectionString);
-            return connection;
-
-        }
-
         public User Find(int id)
         {
-            using (var connection = CreateDbConnection())
+            using (var db = new DatabaseContext())
             {
-                connection.Open();
-
                 const string query = "SELECT * FROM Users WHERE Id = @Id";
 
-                var user = connection.Query<User>(query, new { Id = id }).FirstOrDefault();
+                var user = db.Connection.Query<User>(query, new { Id = id }).FirstOrDefault();
+
                 return user;
             }
         }
 
         public User FindByName(string username)
         {
-            using (var connection = CreateDbConnection())
+            using (var db = new DatabaseContext())
             {
-                connection.Open();
-
                 const string query = "SELECT * FROM Users WHERE EmailAddress = @EmailAddress";
 
-                var user = connection.Query<User>(query, new { EmailAddress = username }).FirstOrDefault();
+                var user = db.Connection.Query<User>(query, new { EmailAddress = username }).FirstOrDefault();
                 return user;
             }
         }
