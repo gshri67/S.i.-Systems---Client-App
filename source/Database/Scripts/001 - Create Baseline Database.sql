d4483f2d10bdf49fcb7a68ec9789a14a16634ea4
Mark Thiessen <mark.thiessen@devfacto.com>
2015-02-04 15:51:05 -0700

    Updates local database scripts to match names and some columns from S.i. database (NOT INCLUDING ALL COLUMNS TO KEEP DEV SCRIPTS SIMPLE. WE CAN ADD MORE AS WE NEED)

--- source/Database/Scripts/001 - Create Baseline Database.sql
+++ source/Database/Scripts/001 - Create Baseline Database.sql
@@ -4,13 +4,39 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 
-CREATE TABLE [dbo].[Users] (
-    [Id]					INT            IDENTITY (1, 1) NOT NULL,
-    [ClientId]				INT            NOT NULL,
-    [FirstName]				NVARCHAR (MAX) NOT NULL,
-    [LastName]				NVARCHAR (MAX) NOT NULL,
-    [EmailAddress]			NVARCHAR (MAX) NOT NULL,
-    [PasswordHash]			NVARCHAR (MAX) NOT NULL,
-    CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED ([Id] ASC)
+CREATE TABLE [dbo].[Company](
+	[CompanyID] [int] IDENTITY(1,1) NOT NULL,
+	[CompanyName] [varchar](150) NOT NULL
+	CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED 
+	(
+		[CompanyID] ASC
+	) ON [PRIMARY]
 );
+GO
+
+CREATE TABLE [dbo].[Users](
+	[UserID] [int] IDENTITY(500,1) NOT NULL,
+	[FirstName] [nvarchar](30) NOT NULL,
+	[Middle] [nvarchar](25) NULL,
+	[LastName] [nvarchar](30) NOT NULL,
+	[UserType] [int] NOT NULL,
+	[CompanyID] [int] NULL,
+	[Inactive] [bit] NOT NULL
+	CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+GO
+
+CREATE TABLE [dbo].[User_Login](
+	[UserID] [int] NOT NULL,
+	[Login] [nvarchar](50) NOT NULL,
+	[Password] [nvarchar](200) NULL
+	CONSTRAINT [PK_UserLogin] PRIMARY KEY CLUSTERED 
+	(
+		[UserID] ASC
+	) ON [PRIMARY],
+	CONSTRAINT [FK_User_Login_Users] FOREIGN KEY ([UserID]) REFERENCES [dbo].[Users] ([UserID])
+	)
+GO
+
+SET ANSI_PADDING OFF
 GO
--- source/Database/Scripts/002 - Add Dev Data.sql
+++ source/Database/Scripts/002 - Add Dev Data.sql
@@ -1,5 +1,50 @@
-﻿INSERT INTO [dbo].[Users] ([ClientId], [FirstName], [LastName], [EmailAddress], [PasswordHash] )
-VALUES (1, 'Bob', 'Smith', 'bob.smith@email.com', 'password')
+﻿SET IDENTITY_INSERT [dbo].[Company] ON;
 
-INSERT INTO [dbo].[Users] ([ClientId], [FirstName], [LastName], [EmailAddress], [PasswordHash] )
-VALUES (2, 'Tom', 'Smith', 'tom.smith@email.com', 'password')
+INSERT INTO [dbo].[Company]
+(
+	[CompanyID], [CompanyName]
+)
+VALUES
+(
+	1, 'Company One'
+);
+
+SET IDENTITY_INSERT [dbo].[Company] OFF;
+
+SET IDENTITY_INSERT [dbo].[Users] ON;
+
+INSERT INTO [dbo].[Users]
+(
+	[UserID]
+    ,[FirstName]
+    ,[Middle]
+    ,[LastName]           
+    ,[UserType]
+    ,[CompanyID]
+	,[Inactive]
+)
+VALUES
+(
+	1
+	,'Bob'
+	,'H.'
+	,'Smith'
+	,490
+	,1
+	,0
+);
+
+SET IDENTITY_INSERT [dbo].[Users] OFF;
+
+INSERT INTO [dbo].[User_Login]
+(
+	[UserID],
+	[Login],
+	[Password]
+)
+VALUES
+(
+	1
+	,'bob.smith@email.com'
+	,'5F4DCC3B5AA765D61D8327DEB882CF99'
+);
