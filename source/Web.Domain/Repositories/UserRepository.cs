4659ae29b1e93cf65943689e9c02f5a90f722d54
Mark Thiessen <mark.thiessen@devfacto.com>
2015-02-05 07:29:05 -0700

    Refactor/simplify UserRepository

--- source/Web.Domain/Repositories/UserRepository.cs
+++ source/Web.Domain/Repositories/UserRepository.cs
@@ -6,15 +6,17 @@ namespace SiSystems.ClientApp.Web.Domain.Repositories
 {
     public class UserRepository
     {
+        const string UserQueryBase = "SELECT [Users].[UserId] Id, [Users].[CompanyID] ClientId, "
+                               + "[Users].[FirstName] FirstName, [Users].[LastName] LastName, "
+                               + "[User_Login].[Login] Login, [User_Login].[Password] PasswordHash "
+                               + "FROM [User_Login], [Users] "
+                               + "WHERE [Users].[UserID]=[User_Login].[UserID] ";
+
         public User Find(int id)
         {
             using (var db = new DatabaseContext())
             {
-                const string query = "SELECT [Users].[UserId] Id, [Users].[CompanyID] ClientId, "
-                                 + "[Users].[FirstName] FirstName, [Users].[LastName] LastName, "
-                                 + "[User_Login].[Login] Login, [User_Login].[Password] PasswordHash "
-                                 + "FROM [User_Login], [Users] "
-                                 + "WHERE [Users].[UserID]=[User_Login].[UserID]"
+                const string query = UserQueryBase
                                  + "AND [Users].[UserId] = @Id";
 
                 var user = db.Connection.Query<User>(query, new { Id = id }).FirstOrDefault();
@@ -27,14 +29,10 @@ namespace SiSystems.ClientApp.Web.Domain.Repositories
         {
             using (var db = new DatabaseContext())
             {
-                const string query = "SELECT [Users].[UserId] Id, [Users].[CompanyID] ClientId, "
-                                 + "[Users].[FirstName] FirstName, [Users].[LastName] LastName, "
-                                 + "[User_Login].[Login] Login, [User_Login].[Password] PasswordHash "
-                                 + "FROM [User_Login], [Users] "
-                                 + "WHERE [Users].[UserID]=[User_Login].[UserID]"
-                                 + "AND [User_Login].[Login] = @Username";
+                const string query = UserQueryBase
+                                     + "AND [User_Login].[Login] = @Username";
 
-                var user = db.Connection.Query<User>(query, new { Username = username }).FirstOrDefault();
+                var user = db.Connection.Query<User>(query, new {Username = username}).FirstOrDefault();
                 return user;
             }
         }
