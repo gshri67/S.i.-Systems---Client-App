67b7141cf50bcddc2e864d024ae4cbd91d90c6ac
Anthony Sopkow <anthonysopkow@gmail.com>
2015-02-05 14:19:56 -0700

    Contracts Controller and View added
    There have also been changes to the pinning of elements on all pages, along with styling changes to better fit with the styleguide

---
+++ source/ClientApp/ClientApp.iOS/ContractorViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace ClientApp.iOS
+{
+	partial class ContractorViewController : UIViewController
+	{
+		public ContractorViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
