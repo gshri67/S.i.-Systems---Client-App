e7fdd1000940d68894fd8936904dd0479e8e3dee
Mark Thiessen <mark.thiessen@devfacto.com>
2015-02-09 10:30:56 -0700

    Renames DB project to Database.Matchguide for clarity

--- developmentScripts/refresh_Dev_Environment.ps1
+++ developmentScripts/refresh_Dev_Environment.ps1
@@ -50,8 +50,8 @@ write-host "Building solution (../buildScripts/_build.ps1)" -foreground "cyan"
 & ../buildScripts/_build.ps1
 write-host
 
-write-host "Running database upgrade tool (../source/Database/bin/Release/SiSystems.ClientApp.Database.exe --create --upgrade)" -foreground "cyan"
-& ../source/Database/bin/Release/SiSystems.ClientApp.Database.exe --create --upgrade
+write-host "Running database upgrade tool (../source/Database/bin/Release/SiSystems.ClientApp.Database.MatchGuide.exe --create --upgrade)" -foreground "cyan"
+& ../source/Database.MatchGuide/bin/Release/SiSystems.ClientApp.Database.MatchGuide.exe --create --upgrade
 
 write-host "Running ClientApp database upgrade tool (../source/Database/bin/Release/SiSystems.ClientApp.ClientAppDb.exe --create --upgrade)" -foreground "cyan"
 & ../source/ClientAppDb/bin/Release/SiSystems.ClientApp.ClientAppDb.exe --create --upgrade
