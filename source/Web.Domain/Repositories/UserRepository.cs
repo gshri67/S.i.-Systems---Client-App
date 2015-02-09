a83d90b1286184b7c35ae43d65aea4813f3ea32d
Mark Thiessen <mark.thiessen@devfacto.com>
2015-02-09 14:58:08 -0700

    Makes UserRepository query a bit more readable.

--- source/Web.Domain/Repositories/UserRepository.cs
+++ source/Web.Domain/Repositories/UserRepository.cs
@@ -6,18 +6,18 @@ namespace SiSystems.ClientApp.Web.Domain.Repositories
 {
     public class UserRepository
     {
-        const string UserQueryBase = "SELECT [Users].[UserId] Id, [Users].[CompanyID] ClientId, "
-                               + "[Users].[FirstName] FirstName, [Users].[LastName] LastName, "
-                               + "[User_Login].[Login] Login, [User_Login].[Password] PasswordHash "
-                               + "FROM [User_Login], [Users] "
-                               + "WHERE [Users].[UserID]=[User_Login].[UserID] ";
+        const string UserQueryBase = "SELECT U.UserId Id, U.CompanyID ClientId, "
+                               + "U.FirstName FirstName, U.LastName LastName, "
+                               + "UL.Login Login, UL.Password PasswordHash "
+                               + "FROM [User_Login] as UL, [Users] as U "
+                               + "WHERE U.UserID=UL.UserID ";
 
         public User Find(int id)
         {
             using (var db = new DatabaseContext(DatabaseSelect.MatchGuide))
             {
                 const string query = UserQueryBase
-                                    + "AND [Users].[UserId] = @Id";
+                                    + "AND U.UserId = @Id";
 
                 return db.Connection.Query<User>(query, new { Id = id }).FirstOrDefault();
             }
@@ -28,7 +28,7 @@ namespace SiSystems.ClientApp.Web.Domain.Repositories
             using (var db = new DatabaseContext(DatabaseSelect.MatchGuide))
             {
                 const string query = UserQueryBase
-                                    + "AND [User_Login].[Login] = @Username";
+                                    + "AND UL.Login = @Username";
 
                 return db.Connection.Query<User>(query, new {Username = username}).FirstOrDefault();
             }
