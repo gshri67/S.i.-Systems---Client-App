c589cb512384bfffb95669c3dd86b455191e00e4
Mark Thiessen <mark.thiessen@devfacto.com>
2015-02-09 10:26:07 -0700

    Adds MatchGuide constants so we can track and document the numbers required by DB queries.

---
+++ source/Web.Domain/Repositories/MatchGuideConstants.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiSystems.ClientApp.Web.Domain.Repositories
+{
+    public class MatchGuideConstants
+    {
+        public class UserTypes
+        {
+            public const int Candidate = 490;
+            public const int ClientContact = 661;
+        }
+
+        public class AgreementTypes
+        {
+            public const int Contract = 459;
+        }
+
+        public class AgreementSubTypes
+        {
+            public const int FloThru = 172;
+        }
+
+        public class ContractStatusTypes
+        {
+            public const int Active = 573;
+            public const int Cancelled = 574;
+            public const int Expired = 575;
+            public const int Pending = 576;
+        }
+    }
+}
