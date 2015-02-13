7e3b222028249d0d7de63a55d71de7cf85c5aab8
mobile.group@devfacto.com <mobile.group@devfacto.com>
2015-02-13 15:55:49 -0700

    Apply version/build to iOS build

--- buildScripts/_ios_build.sh
+++ buildScripts/_ios_build.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+#update version numbers
+build_number=${1:-"0"}
+read -r version < version.txt
+
+find ./source -name 'Info.plist' | while read plist; do
+	/usr/libexec/PlistBuddy -c "Set :CFBundleVersion '${version}.${build_number}'" $plist
+	/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString '${version}'" $plist
+done
+
 #unlock encryption keys for signing
 security default-keychain -s si-systems-xcode.keychain
 security unlock-keychain -p $SI_SIGN_KEY si-systems-xcode.keychain
