9481c9cc7d512b90da71a132b915dbe153c249a0
Curtis Badke <curtis.badke@devfacto.com>
2015-02-19 12:31:10 -0700

    Add Octopus packaging for iOS binaries

---
+++ buildScripts/_ios_package.ps1
@@ -0,0 +1,42 @@
+[CmdletBinding()]
+param (
+)
+
+Import-Module DevFacto.Automation -Force
+
+$scriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
+$scriptName = Split-Path -Leaf $MyInvocation.MyCommand.Path
+$rootDir = Split-Path -Parent $scriptRoot
+$packageDir = "$rootDir/buildResult/"
+
+function PackageBuild
+{
+    param (
+        [String] $configuration
+    )
+
+    pushd $rootDir\source\ClientApp\ClientApp.iOS\Octopus
+
+    copy $packageDir\*$configuration.ipa .\
+
+    ls *.ipa | % { $_.Name -match '.*(?<version>\d+\.\d+\.\d+\.\d+).*'}
+
+    Update-Nuspec -FilePath ".\$configuration.nuspec" -Version $Matches['version']
+
+    nuget pack ".\$configuration.nuspec"
+
+    mv *.nupkg $packageDir\
+
+    rm *.ipa
+
+    popd
+}
+
+$ErrorActionPreference = "Stop"
+
+Write-Verbose ("Executing " + $scriptName)
+
+PackageBuild "Test"
+PackageBuild "Release"
+
+Write-Verbose ($scriptName + " complete")
