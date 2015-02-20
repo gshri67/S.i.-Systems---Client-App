9103c0ab1d99956a26d1334c1e5af5d649dbd929
Curtis Badke <curtis.badke@devfacto.com>
2015-02-20 11:43:06 -0700

    Revert "Add target name to long build number"
    iTunesConnect says this is an invalid version number. Need a build quick. Will do more research.
    
    This reverts commit eb02cf1e73b4899713cac3c9e1b0958c9ccda0c2.

--- buildScripts/_ios_build.sh
+++ buildScripts/_ios_build.sh
@@ -6,6 +6,10 @@ resultsDir=./buildResult
 build_number=${1:-"0"}
 read -r version < version.txt
 
+find ./source -name 'Info.plist' | while read plist; do
+	/usr/libexec/PlistBuddy -c "Set :CFBundleVersion '${version}.${build_number}'" $plist
+	/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString '${version}'" $plist
+done
 
 #unlock encryption keys for signing
 security default-keychain -s si-systems-xcode.keychain
@@ -14,11 +18,6 @@ security unlock-keychain -p $SI_SIGN_KEY si-systems-xcode.keychain
 mkdir $resultsDir
 
 function CreateBuild {
-	find ./source -name 'Info.plist' | while read plist; do
-		/usr/libexec/PlistBuddy -c "Set :CFBundleVersion '${version}.${build_number}.${1}'" $plist
-		/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString '${version}'" $plist
-	done
-
 	/Applications/Xamarin\ Studio.app/Contents/MacOS/mdtool -v build "--configuration:${1}|iPhone" ./source/SiSystems.ClientApp.sln
 
 	find ./source -path "*/${1}/*" -name "*.ipa" | while read package; do
