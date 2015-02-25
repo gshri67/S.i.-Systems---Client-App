316e2016b97d4661171cf1ae746ca0f657f9ba46
Craig Cairns <craig.cairns@devfacto.com>
2015-02-25 16:34:00 -0700

    Adds service to get a specializations.

---
+++ source/Web.Domain/Repositories/SpecializationRepository.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Dapper;
+using SiSystems.ClientApp.SharedModels;
+
+namespace SiSystems.ClientApp.Web.Domain.Repositories
+{
+    public interface ISpecializationRepository
+    {
+        Task<IEnumerable<Specialization>> GetAllAsync();
+    }
+
+    public class SpecializationRepository: ISpecializationRepository
+    {
+        public async Task<IEnumerable<Specialization>> GetAllAsync()
+        {
+            using (var db = new DatabaseContext(DatabaseSelect.MatchGuide))
+            {
+                return await db.Connection.QueryAsync<Specialization>("SELECT SpecializationId Id, Name, Description FROM Specialization");
+            }
+        }
+
+        public async Task<IEnumerable<Specialization>> FindAsync(int id)
+        {
+            using (var db = new DatabaseContext(DatabaseSelect.MatchGuide))
+            {
+                return
+                    await
+                        db.Connection.QueryAsync<Specialization>(
+                            "SELECT SpecializationId Id, Name, Description FROM Specialization WHERE SpecializationId = @Id",
+                            new {Id = id});
+            }
+        }
+    }
+}
---
+++ source/Web.Domain/Services/SpecializationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiSystems.ClientApp.SharedModels;
+using SiSystems.ClientApp.Web.Domain.Repositories;
+
+namespace SiSystems.ClientApp.Web.Domain.Services
+{
+    public class SpecializationService
+    {
+        private readonly ISpecializationRepository _specializationRepository;
+
+        public SpecializationService(ISpecializationRepository specializationRepository)
+        {
+            this._specializationRepository = specializationRepository;
+        }
+
+        public async Task<IEnumerable<Specialization>> GetAllAsync()
+        {
+            return await _specializationRepository.GetAllAsync();
+        }
+    }
+}
