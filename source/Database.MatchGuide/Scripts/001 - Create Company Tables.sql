80776981a51e1d726d675cf9e4f1f2e44448a9de
Craig Cairns <craig.cairns@devfacto.com>
2015-02-27 15:29:25 -0700

    Adds new columns to MatchGuideDev DB to mimic the columns that were added to MatchGuide by Chennai. Adds in columns, lookups and data for Invoice Format and Invoice Frequency.

--- source/Database.MatchGuide/Scripts/001 - Create Company Tables.sql
+++ source/Database.MatchGuide/Scripts/001 - Create Company Tables.sql
@@ -1,38 +1,82 @@
 ﻿
-CREATE TABLE [dbo].[Company](
-	[CompanyID] [int] IDENTITY(1,1) NOT NULL,
-	[CompanyName] [varchar](150) NOT NULL,
-	--[StatusType] [int] NULL,
-	--[EstimatedCandidates] [int] NULL CONSTRAINT [DF_Company_EstimatedCandidates]  DEFAULT (0),
-	--[MainAreaCode] [decimal](3, 0) NULL,
-	--[MainPhoneNumber] [decimal](7, 0) NULL,
-	--[IndustryType] [int] NULL,
-	--[LegalID] [int] NULL,
-	--[WebSite] [varchar](100) NULL,
-	--[CreateDate] [smalldatetime] NOT NULL CONSTRAINT [DF_Company_CreateDate]  DEFAULT (getdate()),
-	--[CreateUserID] [int] NOT NULL,
-	--[UpdateDate] [smalldatetime] NULL,
-	--[UpdateUserID] [int] NULL,
-	--[Inactive] [bit] NOT NULL CONSTRAINT [DF_Company_Inactive]  DEFAULT (0),
-	--[PAM] [bit] NULL,
-	--[GST_Applicable] [bit] NOT NULL CONSTRAINT [DF_Company_GST_Applicable]  DEFAULT ((1)),
-	--[FTFeeType] [int] NULL,
-	--[FTFeeValue] [money] NULL,
-	--[FTFloorRate] [money] NULL,
-	--[taxapplicable] [int] NULL,
-	--[IndustryId] [int] NULL,
-	--[SubIndustryId] [int] NULL,
-	--[InvoiceFormatId] [int] NULL,
-	--[InvoiceFrequencyId] [int] NULL,
-	--[HasProjectPO] [bit] NULL,
-	--[LimitationOfContract] [bit] NOT NULL DEFAULT ((0)),
-	--[SendEPerformance] [bit] NOT NULL CONSTRAINT [df_company_sendeperformance]  DEFAULT ((1)),
-	--[TargetPayTime] [int] NULL DEFAULT ((0)),
- CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED 
-(
-	[CompanyID] ASC
-) ON [PRIMARY]
+CREATE TABLE [dbo].[Company] (
+    [CompanyID]            INT           IDENTITY (1, 1) NOT NULL,
+    [CompanyName]          VARCHAR (150) NOT NULL,
+    --[StatusType]           INT           NULL,
+    --[EstimatedCandidates]  INT           NULL,
+    --[MainAreaCode]         DECIMAL (3)   NULL,
+    --[MainPhoneNumber]      DECIMAL (7)   NULL,
+    --[IndustryType]         INT           NULL,
+    --[LegalID]              INT           NULL,
+    --[WebSite]              VARCHAR (100) NULL,
+    --[CreateDate]           SMALLDATETIME NOT NULL,
+    --[CreateUserID]         INT           NOT NULL,
+    --[UpdateDate]           SMALLDATETIME NULL,
+    --[UpdateUserID]         INT           NULL,
+    --[Inactive]             BIT           NOT NULL,
+    --[PAM]                  BIT           NULL,
+    --[GST_Applicable]       BIT           NOT NULL,
+    --[FTFeeType]            INT           NULL,
+    --[FTFeeValue]           MONEY         NULL,
+    --[FTFloorRate]          MONEY         NULL,
+    --[taxapplicable]        INT           NULL,
+    --[IndustryId]           INT           NULL,
+    --[SubIndustryId]        INT           NULL,
+    [InvoiceFormatId]      INT           NULL,
+    [InvoiceFrequencyId]   INT           NULL,
+    --[HasProjectPO]         BIT           NULL,
+    --[LimitationOfContract] BIT           NOT NULL,
+    --[SendEPerformance]     BIT           NOT NULL,
+    --[TargetPayTime]        INT           NULL,
+
+	[MSPFeePercentage] DECIMAL(6,2) DEFAULT NULL,
+	[FloThruFeeType] INT DEFAULT NULL,
+	[FloThruFee] DECIMAL(6,2) DEFAULT NULL,
+	[FloThruFeePayment] INT DEFAULT NULL,
+	[FloThruMSPPayment] INT DEFAULT NULL,
+	--[InvoiceFrequency] INT DEFAULT NULL,
+	[FloThruAlumni] BIT DEFAULT NULL,
+	[MaxVisibleRatePerHour] MONEY DEFAULT NULL,
+	[MaxVisibleRatePerDay] MONEY DEFAULT NULL,
+    CONSTRAINT [PK_Clients] PRIMARY KEY CLUSTERED ([CompanyID] ASC) WITH (FILLFACTOR = 97)
 );
+
+GO
+
+CREATE TABLE [dbo].[PAMInvoiceFormat] (
+    [InvoiceFormatId] INT          IDENTITY (1, 1) NOT NULL,
+    [Title]           VARCHAR (50) NOT NULL,
+    --[DisplayOrder]    SMALLINT     NOT NULL,
+    --[GPCode]          VARCHAR (25) NOT NULL,
+    CONSTRAINT [PK_PAMInvoiceFormatDetails] PRIMARY KEY CLUSTERED ([InvoiceFormatId] ASC)
+);
+
+
+GO
+
+CREATE TABLE [dbo].[PAMInvoiceFrequency] (
+    [InvoiceFrequencyId] INT          IDENTITY (1, 1) NOT NULL,
+    [Title]              VARCHAR (50) NOT NULL,
+    --[DisplayOrder]       SMALLINT     NOT NULL,
+    --[GPCode]             VARCHAR (25) NOT NULL,
+    CONSTRAINT [PK_PAMInvoiceFrequencyDetails] PRIMARY KEY CLUSTERED ([InvoiceFrequencyId] ASC)
+);
+
+GO
+ALTER TABLE [dbo].[Company] WITH NOCHECK
+    ADD CONSTRAINT [FK_Company_InvoiceFormatId] FOREIGN KEY ([InvoiceFormatId]) REFERENCES [dbo].[PAMInvoiceFormat] ([InvoiceFormatId]);
+
+GO
+
+ALTER TABLE [dbo].[Company] WITH NOCHECK
+    ADD CONSTRAINT [FK_Company_InvoiceFrequencyId] FOREIGN KEY ([InvoiceFrequencyId]) REFERENCES [dbo].[PAMInvoiceFrequency] ([InvoiceFrequencyId]);
+
+GO
+
+ALTER TABLE [dbo].[Company] WITH CHECK CHECK CONSTRAINT [FK_Company_InvoiceFormatId];
+
+ALTER TABLE [dbo].[Company] WITH CHECK CHECK CONSTRAINT [FK_Company_InvoiceFrequencyId];
+
 
 GO
 
@@ -56,4 +100,18 @@ GO
 
 ALTER TABLE [dbo].[Company_ParentChildRelationship]  WITH NOCHECK ADD  CONSTRAINT [FK_ClientRelationships_Companies1] FOREIGN KEY([ChildID])
 REFERENCES [dbo].[Company] ([CompanyID])
+GO
+
+CREATE TABLE [dbo].[CompanyDomain] (
+	CompanyDomainID	INT	NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	CompanyID INT NOT NULL	,
+	EmailDomain NVARCHAR(255) NOT NULL,
+	--CreatedUserId INT NOT NULL,
+	--CreatedDateTime DATETIME DEFAULT GETDATE(),
+	--UpdatedUserId INT,
+	--UpdatedDateTime DATETIME,
+	IsActive BIT DEFAULT 1
+	CONSTRAINT FKComDomCompanyId FOREIGN KEY (CompanyID) REFERENCES Company(CompanyID)
+);
+
 GO
--- source/Database.MatchGuide/Scripts/010 - Add Companies.sql
+++ source/Database.MatchGuide/Scripts/010 - Add Companies.sql
@@ -23,3 +23,65 @@ VALUES
 
 
 SET IDENTITY_INSERT [dbo].[Company_ParentChildRelationship] OFF;
+
+SET IDENTITY_INSERT [dbo].[CompanyDomain] ON;
+
+INSERT INTO [dbo].[CompanyDomain]
+(
+	CompanyDomainID, CompanyID, EmailDomain
+)
+VALUES
+(1, 1, 'companyone'),
+(2, 2, 'companytwo'),
+(3, 3, 'companythree'),
+(4, 4, 'division1.companyone');
+
+SET IDENTITY_INSERT [dbo].[CompanyDomain] OFF;
+
+
+SET IDENTITY_INSERT [dbo].[PAMInvoiceFormat] ON
+
+INSERT INTO [dbo].[PAMInvoiceFormat] ([InvoiceFormatId], [Title]) 
+VALUES 
+(1, N'1 Credit per Client'),
+(2, N'1 Credit per Consultant/PO'),
+(3, N'1 Credit per PO or Project'),
+(4, N'1 Credit per Project/PO Multi'),
+(5, N'1 Credit per Reporting Manager'),
+(6, N'1 Credit pr Contract (Job)'),
+(7, N'1 Invoice per Client'),
+(8, N'1 Invoice per Consultant/PO'),
+(9, N'1 Invoice per Contract (Job)'),
+(10, N'1 Invoice per Perm'),
+(11, N'1 Invoice per PO or Project'),
+(12, N'1 Invoice per Project/ PO Mult'),
+(13, N'1 Invoice per Reporting Manage'),
+(14, N'Contracts Renewed'),
+(15, N'credit'),
+(16, N'Credit for Consultant Renewal'),
+(17, N'CREDIT FOR FED GOV INV'),
+(18, N'Fed Govern with SI Systems'),
+(19, N'Invoice information section'),
+(20, N'One Credit per Temporary ID'),
+(21, N'One Invoice per Temporary ID'),
+(22, N'opamperm'),
+(23, N'Perm Credit Note'),
+(24, N'Weekly Invoicing'),
+(25, N'Weekly Invoicing Credit'),
+(26, N'PAM Perm Credit Note'),
+(27, N'1 invoice per job'),
+(28, N'1 invoice per job'),
+(29, N'Nexstaf 1 Credit per Job'),
+(30, N'Nexstaf 1 Invoice per Job')
+
+SET IDENTITY_INSERT [dbo].[PAMInvoiceFormat] OFF
+
+SET IDENTITY_INSERT [dbo].[PAMInvoiceFrequency] ON
+
+INSERT INTO [dbo].[PAMInvoiceFrequency] ([InvoiceFrequencyId], [Title]) 
+VALUES
+(1, 'Monthly'),
+(2, 'Semi Monthly'),
+(3, 'Weekly Invoicing')
+
+SET IDENTITY_INSERT [dbo].[PAMInvoiceFrequency] OFF
