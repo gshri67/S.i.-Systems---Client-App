642b6249254a50ddd162114e0028ba468bb4de4f
Craig Cairns <craig.cairns@devfacto.com>
2015-03-06 16:00:10 -0700

    Adds a webapi endpoint to access the forgot password service.

---
+++ source/Web.Domain/Services/AccountService.cs
@@ -0,0 +1,27 @@
+﻿using SiSystems.ClientApp.Web.Domain.MatchGuideService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiSystems.ClientApp.Web.Domain.Services
+{
+    public class AccountService
+    {
+        const string PortalName = "Client";
+
+        private readonly IAccountService _client;
+
+        public AccountService(IAccountService client)
+        {
+            this._client = client;
+        }
+
+        public async Task<bool> ForgotPassword(string emailAddress)
+        {
+            var response = await this._client.ForgotPasswordAsync(emailAddress, PortalName);
+            return true;
+        }
+    }
+}
