36b05fb12ba8bb9b0570b226e980a7b4818ee40b
Craig Cairns <craig.cairns@devfacto.com>
2015-03-10 13:34:56 -0600

    Removes the shared code project as the functionality is now in the Core PCL

---
+++ source/ClientApp.Core.Tests/MatchGuideConstantsTests.cs
@@ -0,0 +1,124 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using SiSystems.ClientApp.SharedModels;
+
+namespace SiSystems.ClientApp.Core.Tests
+{
+    [TestFixture]
+    public class MatchGuideConstantsTests
+    {
+        public class FakeObject
+        {
+            public MatchGuideConstants.ResumeRating Rating;
+
+            public MatchGuideConstants.ResumeRating? NullableRating;
+        }
+
+        [Test]
+        public void ResumeRating_FromJsonInt_CanBeDeserialized()
+        {
+            const string jsonString = "{\"rating\":317}";
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+
+            Assert.AreEqual(317, sut.Rating);
+        }
+
+        [Test]
+        [ExpectedException]
+        public void ResumeRating_FromJsonStringToNonNullableProperty_ShouldFail()
+        {
+            const string jsonString = "{\"rating\":\"317\"}";
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+        }
+
+        [Test]
+        [ExpectedException]
+        public void ResumeRating_FromJsonStringToNullableProperty_ShouldFail()
+        {
+            const string jsonString = "{\"nullableRating\":\"317\"}";
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+        }
+
+        [Test]
+        public void ResumeRating_FromJsonNullToNullableProperty_CanBeDeserialized()
+        {
+            const string jsonString = "{\"nullableRating\":null}";
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+
+            Assert.IsFalse(sut.NullableRating.HasValue);
+        }
+
+        [Test]
+        [ExpectedException]
+        public void ResumeRating_FromJsonNullToNonNullableProperty_ShouldFail()
+        {
+            const string jsonString = "{\"rating\":null}";
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+        }
+
+        [Test]
+        public void ResumeRating_ToString_ShouldDisplayDefaultString()
+        {
+            const string jsonString = "{\"rating\":0}";
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+
+            Assert.AreEqual("Not Checked", sut.Rating.ToString());
+        }
+
+        [Test]
+        public void ResumeRating_ToString_ShouldDisplayStandardString()
+        {
+            string jsonString = string.Format("{{\"rating\":{0}}}", MatchGuideConstants.ResumeRating.Standard);
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+
+            Assert.AreEqual("Standard", sut.Rating.ToString());
+        }
+
+        [Test]
+        public void ResumeRating_ToString_ShouldDisplayAboveStandardString()
+        {
+            string jsonString = string.Format("{{\"rating\":{0}}}", MatchGuideConstants.ResumeRating.AboveStandard);
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+
+            Assert.AreEqual("Above Standard", sut.Rating.ToString());
+        }
+
+        [Test]
+        public void ResumeRating_ToString_ShouldDisplayBelowStandardString()
+        {
+            string jsonString = string.Format("{{\"rating\":{0}}}", MatchGuideConstants.ResumeRating.BelowStandard);
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+
+            Assert.AreEqual("Below Standard", sut.Rating.ToString());
+        }
+
+        [Test]
+        public void ResumeRating_ToString_ShouldDisplayDefaultStringWhenUnknownValue()
+        {
+            string jsonString = string.Format("{{\"rating\":{0}}}", 1337);
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+
+            Assert.AreEqual("Not Checked", sut.Rating.ToString());
+        }
+
+        [Test]
+        public void ResumeRating_ShouldHandleNullForNullableProperties()
+        {
+            string jsonString = string.Format("{{\"nullableRating\":{0}}}", "null");
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+
+            Assert.IsFalse(sut.NullableRating.HasValue);
+            Assert.IsNull(sut.NullableRating);
+        }
+
+        [Test]
+        public void ResumeRating_ShouldHandleValueForNullableProperties()
+        {
+            string jsonString = string.Format("{{\"nullableRating\":{0}}}", MatchGuideConstants.ResumeRating.Standard);
+            var sut = JsonConvert.DeserializeObject<FakeObject>(jsonString);
+
+            Assert.IsTrue(sut.NullableRating.HasValue);
+            Assert.AreEqual(MatchGuideConstants.ResumeRating.Standard, sut.NullableRating.Value);
+        }
+    }
+}
---
+++ source/ClientApp.Core.Tests/ViewModels/EulaViewModelTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using ClientApp.Core.ViewModels;
+
+namespace SiSystems.ClientApp.Core.Tests.ViewModels
+{
+    [TestFixture]
+    public class EulaViewModelTests
+    {
+        [Test]
+        public void AcceptEula_AddsUser()
+        {
+            var vm = new EulaViewModel(new Dictionary<string, int>());
+            vm.AcceptEula("testuser", 1);
+
+            Assert.AreEqual("{\"testuser\":1}", vm.GetUpdatedStorageString());
+        }
+
+        [Test]
+        public void AcceptEula_UpdatesOldUser()
+        {
+            var dic = new Dictionary<string, int> {{"testuser", 1}};
+            var vm = new EulaViewModel(dic);
+            vm.AcceptEula("testuser", 2);
+
+            Assert.AreEqual("{\"testuser\":2}", vm.GetUpdatedStorageString());
+        }
+
+        [Test]
+        public void AcceptEula_LeavesOtherUsers()
+        {
+            var dic = new Dictionary<string, int> { { "testuser", 1 } };
+            var vm = new EulaViewModel(dic);
+            vm.AcceptEula("newUser", 2);
+
+            Assert.AreEqual("{\"testuser\":1,\"newUser\":2}", vm.GetUpdatedStorageString());
+        }
+    }
+}
