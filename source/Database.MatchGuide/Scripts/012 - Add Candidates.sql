40bc62b9edf9065eb22894fcab6536076ad1aff1
Craig Cairns <craig.cairns@devfacto.com>
2015-03-11 01:02:56 -0600

    Removes the company id from Candiate user types as that type of association does not exist in Match Guide.

--- source/Database.MatchGuide/Scripts/012 - Add Candidates.sql
+++ source/Database.MatchGuide/Scripts/012 - Add Candidates.sql
@@ -4,17 +4,17 @@ SET IDENTITY_INSERT [dbo].[Users] ON;
 
 INSERT INTO [dbo].[Users]
 (
-	[UserID], [FirstName], [Middle], [LastName], [UserType], [CompanyID], [Inactive]
+	[UserID], [FirstName], [Middle], [LastName], [UserType], [Inactive]
 )
 VALUES
 -- Contracts With Company One Only
-(10, 'Tommy', 'H.', 'Contractor', 490, 1, 0),
+(10, 'Tommy', 'H.', 'Contractor', 490, 0),
 -- Contracts With Company Two Only
-(11, 'Bill', 'H.','Contractasaurus', 490, 2, 0),
+(11, 'Bill', 'H.','Contractasaurus', 490, 0),
 -- Active With Comp Three And Alumni With One
-(12, 'Candice', 'J.','Consulty', 490, 3, 0),
+(12, 'Candice', 'J.','Consulty', 490, 0),
 -- Alumni with division of company one
-(13, 'Sally', 'P', 'Divisioner', 490, 4, 0);
+(13, 'Sally', 'P', 'Divisioner', 490, 0);
 
 SET IDENTITY_INSERT [dbo].[Users] OFF;
 
