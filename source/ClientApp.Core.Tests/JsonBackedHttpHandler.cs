95caead31957df4528ce160c5ac4f280f848fcff
Craig Cairns <craig.cairns@devfacto.com>
2015-03-20 14:50:35 -0600

    Added some additional simple tests for the APIClient.

---
+++ source/ClientApp.Core.Tests/JsonBackedHttpHandler.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApp.Core.Tests
+{
+    public class JsonBackedHandler : DelegatingHandler
+    {
+        readonly Dictionary<string, object> DataSource;
+
+        internal JsonBackedHandler(string filename)
+        {
+            this.DataSource = JsonConvert.DeserializeObject<Dictionary<string, object>>(File.ReadAllText(filename));
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+        {
+            var data = DataSource[request.RequestUri.AbsolutePath].ToString();
+
+            return Task.FromResult(new HttpResponseMessage(System.Net.HttpStatusCode.OK) { Content = new StringContent(data) });
+        }
+    }
+}
