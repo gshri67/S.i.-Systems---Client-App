f6e42e96c814a0e115a8b7fcd313d2ea03a5da76
Rory Fuhrer <rory.fuhrer@devfacto.com>
2015-03-24 11:03:17 -0600

    Cleaned up unused references in the client

--- source/ClientApp.Core/ViewModels/OnboardViewModel.cs
+++ source/ClientApp.Core/ViewModels/OnboardViewModel.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 using SiSystems.ClientApp.SharedModels;
 
 namespace ClientApp.Core.ViewModels
