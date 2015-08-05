a5f965df19dc2ccb018a77de2da3af7332bf50c2
FarazBhojani <faraz.bhojani@devfacto.com>
2015-08-05 13:19:29 -0600

    Create basic layout for Time entry: More or less done the basic layout.

---
+++ source/App2/DailyCalendarHeaderView.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace App2
+{
+	partial class DailyCalendarHeaderView : UIView
+	{
+		public DailyCalendarHeaderView (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
