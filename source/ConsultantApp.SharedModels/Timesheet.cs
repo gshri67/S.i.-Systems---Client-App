0e8657e5c192f1dd292b32a67fc3296b5d248097
Anthony Sopkow <anthonysopkow@gmail.com>
2015-08-06 15:23:23 -0600

    Getter Always Returns

--- source/ConsultantApp.SharedModels/Timesheet.cs
+++ source/ConsultantApp.SharedModels/Timesheet.cs
@@ -13,7 +13,6 @@ namespace ConsultantApp.SharedModels
 		{
 			get
 			{ 
-				if (startDate != null && endDate != null)
 					return startDate.ToString("MMM dd") + "-" + endDate.ToString("MMM dd");
 			}
 		}
