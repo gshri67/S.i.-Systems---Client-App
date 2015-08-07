f10891c21738d95a0e860009594f4f61971d287f
FarazBhojani <faraz.bhojani@devfacto.com>
2015-08-07 08:45:27 -0600

    Implement picker views for adding entries: Basic Image pickers added

--- source/ConsultantApp/ConsultantApp.iOS/TimeEntryViewController/AddTimeEntryViewController/AddTimeEntryViewController.cs
+++ source/ConsultantApp/ConsultantApp.iOS/TimeEntryViewController/AddTimeEntryViewController/AddTimeEntryViewController.cs
@@ -5,8 +5,15 @@ namespace ConsultantApp.iOS.TimeEntryViewController.AddTimeEntryViewController
 {
 	partial class AddTimeEntryViewController : UIViewController
 	{
+		private UIPickerView clientPickerView;
+		private UIPickerView projectCodePickerView;
+		private PickerViewModel pickerViewModel;
+
 		public AddTimeEntryViewController (IntPtr handle) : base (handle)
 		{
+			clientPickerView = new UIPickerView (new CoreGraphics.CGRect(0, 0, 300, 300));
+			projectCodePickerView = new UIPickerView (new CoreGraphics.CGRect(0, 0, 300, 300));
+			pickerViewModel = new PickerViewModel ();
 		}
 
 		public override void ViewDidLoad ()
@@ -17,6 +24,41 @@ namespace ConsultantApp.iOS.TimeEntryViewController.AddTimeEntryViewController
 				//DismissViewController(true, null);
 				NavigationController.PopViewController(true);
 			};
+
+			clientTextField.InputView = clientPickerView;
+			projectCodeTextField.InputView = projectCodePickerView;
+
+			clientPickerView.Model = pickerViewModel;
+			projectCodePickerView.Model = pickerViewModel;
+		}
+
+		//dismiss keyboard when tapping outside of text fields
+		public override void TouchesBegan(NSSet touches, UIEvent evt)
+		{
+			base.TouchesBegan(touches, evt);
+
+			clientTextField.ResignFirstResponder();
+			projectCodeTextField.ResignFirstResponder();
+			hoursTextField.ResignFirstResponder();
+		}
+
+		public class PickerViewModel : UIPickerViewModel
+		{
+			public override nint GetComponentCount (UIPickerView picker)
+			{
+				return 1;
+			}
+
+			public override nint GetRowsInComponent (UIPickerView picker, nint component)
+			{
+				return 5;
+			}
+
+			public override string GetTitle (UIPickerView pickerView, nint row, nint component)
+			{
+
+				return "Component " + row.ToString();
+			}
 		}
 	}
 }
