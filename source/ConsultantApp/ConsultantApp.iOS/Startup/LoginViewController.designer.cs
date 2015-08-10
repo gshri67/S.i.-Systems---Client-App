9657bb5e0b48e02724eb75e28b63b741869e1741
Anthony Sopkow <anthonysopkow@gmail.com>
2015-08-10 13:31:32 -0600

    Changing the names of some values on the login screen to match that in the loginViewController

--- source/ConsultantApp/ConsultantApp.iOS/Startup/LoginViewController.designer.cs
+++ source/ConsultantApp/ConsultantApp.iOS/Startup/LoginViewController.designer.cs
@@ -4,86 +4,79 @@
 // actions declared in your storyboard file.
 // Manual changes to this file will not be maintained.
 //
+using ConsultantApp.iOS;
 using Foundation;
 using System;
 using System.CodeDom.Compiler;
-using ConsultantApp.iOS;
 using UIKit;
 
 namespace ConsultantApp.iOS
 {
-    [Register("LoginViewController")]
-    partial class LoginViewController
-    {
-        [Outlet]
-        [GeneratedCode("iOS Designer", "1.0")]
-        UIButton ForgotPasswordButton { get; set; }
+	[Register ("LoginViewController")]
+	partial class LoginViewController
+	{
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton ForgotPasswordButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode("iOS Designer", "1.0")]
-        UIButton login { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIButton login { get; set; }
 
-        [Outlet]
-        [GeneratedCode("iOS Designer", "1.0")]
-        UIActivityIndicatorView loginActivityIndicator { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIActivityIndicatorView loginActivityIndicator { get; set; }
 
-        [Outlet]
-        [GeneratedCode("iOS Designer", "1.0")]
-        UIView LoginView { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIView LoginView { get; set; }
 
-        [Outlet]
-        [GeneratedCode("iOS Designer", "1.0")]
-        UITextField password { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField password { get; set; }
 
-        [Outlet]
-        [GeneratedCode("iOS Designer", "1.0")]
-        BorderedButton SignUpButton { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		ConsultantApp.iOS.BorderedButton SignUpButton { get; set; }
 
-        [Outlet]
-        [GeneratedCode("iOS Designer", "1.0")]
-        UITextField username { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UITextField username { get; set; }
 
-        [Action("login_TouchUpInside:")]
-        [GeneratedCode("iOS Designer", "1.0")]
-        partial void login_TouchUpInside(UIButton sender);
+		[Action ("login_TouchUpInside:")]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		partial void login_TouchUpInside (UIButton sender);
 
-        void ReleaseDesignerOutlets()
-        {
-            if (ForgotPasswordButton != null)
-            {
-                ForgotPasswordButton.Dispose();
-                ForgotPasswordButton = null;
-            }
-            if (login != null)
-            {
-                login.Dispose();
-                login = null;
-            }
-            if (loginActivityIndicator != null)
-            {
-                loginActivityIndicator.Dispose();
-                loginActivityIndicator = null;
-            }
-            if (LoginView != null)
-            {
-                LoginView.Dispose();
-                LoginView = null;
-            }
-            if (password != null)
-            {
-                password.Dispose();
-                password = null;
-            }
-            if (SignUpButton != null)
-            {
-                SignUpButton.Dispose();
-                SignUpButton = null;
-            }
-            if (username != null)
-            {
-                username.Dispose();
-                username = null;
-            }
-        }
-    }
+		void ReleaseDesignerOutlets ()
+		{
+			if (ForgotPasswordButton != null) {
+				ForgotPasswordButton.Dispose ();
+				ForgotPasswordButton = null;
+			}
+			if (login != null) {
+				login.Dispose ();
+				login = null;
+			}
+			if (loginActivityIndicator != null) {
+				loginActivityIndicator.Dispose ();
+				loginActivityIndicator = null;
+			}
+			if (LoginView != null) {
+				LoginView.Dispose ();
+				LoginView = null;
+			}
+			if (password != null) {
+				password.Dispose ();
+				password = null;
+			}
+			if (SignUpButton != null) {
+				SignUpButton.Dispose ();
+				SignUpButton = null;
+			}
+			if (username != null) {
+				username.Dispose ();
+				username = null;
+			}
+		}
+	}
 }
--- source/ConsultantApp/ConsultantApp.iOS/TimeEntryViewController/TimeSheetEntryViewController.designer.cs
+++ source/ConsultantApp/ConsultantApp.iOS/TimeEntryViewController/TimeSheetEntryViewController.designer.cs
@@ -1,11 +1,13 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
-
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
 namespace ConsultantApp.iOS.TimeEntryViewController
 {
@@ -14,13 +16,9 @@ namespace ConsultantApp.iOS.TimeEntryViewController
 	{
 		[Outlet]
 		UIKit.UIButton addButton { get; set; }
-		
+
 		void ReleaseDesignerOutlets ()
 		{
-			if (addButton != null) {
-				addButton.Dispose ();
-				addButton = null;
-			}
 		}
 	}
 }
