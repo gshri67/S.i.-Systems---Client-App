59761c5c2dfbba6f5bc125b8b00c9b96d034ac5a
Anthony Sopkow <anthonysopkow@gmail.com>
2015-09-03 15:03:04 -0600

    Adding TimeSheetDetails to the local DB

--- source/Database.MatchGuide/Scripts/016 - Add Timesheets.sql
+++ source/Database.MatchGuide/Scripts/016 - Add Timesheets.sql
@@ -16,7 +16,38 @@ INSERT INTO [dbo].[TimeSheet]
            ,[TimeSheetAvailablePeriodID]
            ,[StatusID])
      VALUES
-			--Candice, Jul 1 - 31, Open
+			--Candice, Jul 1 - 31, Submitted
             (12, 4, 3, 622)
-		    --Candice, Jul 1 - 31, Open
+		    --Candice, Aug 1 - 31, Open
 		   ,(12, 4, 1, 621);
+
+INSERT INTO [dbo].[TimeSheetDetail]
+           ([ContractRateID]
+           ,[PONumber]
+           ,[ProjectID]
+           ,[Day]
+           ,[UnitValue]
+           ,[Description]
+           ,[TimesheetID])
+     VALUES
+           (4, null, null, 3, 8, null, 2)
+		   ,(4, null, null, 4, 8, null, 2)
+		   ,(4, null, null, 5, 8, null, 2)
+		   ,(4, null, null, 6, 8, null, 2)
+		   ,(4, null, null, 7, 8, null, 2)
+		   ,(4, null, null, 10, 8, null, 2)
+		   ,(4, null, null, 11, 8, null, 2)
+		   ,(4, null, null, 12, 8, null, 2)
+		   ,(4, null, null, 13, 8, null, 2)
+		   ,(4, null, null, 14, 8, null, 2)
+		   ,(4, null, null, 17, 8, null, 2)
+		   ,(4, null, null, 18, 8, null, 2)
+		   ,(4, null, null, 19, 8, null, 2)
+		   ,(4, null, null, 20, 8, null, 2)
+		   ,(4, null, null, 21, 8, null, 2)
+		   ,(4, null, null, 24, 8, null, 2)
+		   ,(4, null, null, 25, 8, null, 2)
+		   ,(4, null, null, 26, 8, null, 2)
+		   ,(4, null, null, 27, 8, null, 2)
+		   ,(4, null, null, 28, 8, null, 2)
+		   ,(4, null, null, 31, 8, null, 2);
