7d870ff38d4358ca9772784392b4f109cdb529e7
Anthony Sopkow <anthonysopkow@gmail.com>
2015-09-03 17:13:32 -0600

    TimeSheetRepository Accesses Database

--- source/Web.Domain/Repositories/TimesheetRepository.cs
+++ source/Web.Domain/Repositories/TimesheetRepository.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using Dapper;
+using SiSystems.ClientApp.Web.Domain;
+using SiSystems.ClientApp.Web.Domain.Repositories;
 using SiSystems.ClientApp.Web.Domain.Repositories.Search;
 using SiSystems.SharedModels;
 
@@ -9,21 +11,36 @@ namespace SiSystems.ConsultantApp.Web.Domain.Repositories
 {
     public interface ITimesheetRepository
     {
-        IEnumerable<Timesheet> GetTimesheetsForUser(int id);
+        IEnumerable<Timesheet> GetTimesheetsForUser(int userId);
         Timesheet CreateTimesheet(Timesheet timesheet);
         Timesheet UpdateTimesheet(Timesheet timesheet);
     }
 
     public class TimesheetRepository : ITimesheetRepository
     {
-        public IEnumerable<Timesheet> GetTimesheetsForDate(DateTime date)
+        public IEnumerable<Timesheet> GetTimesheetsForUser(int userId)
         {
-            return UserTimesheets;
-        }
+            using (var db = new DatabaseContext(DatabaseSelect.MatchGuide))
+            {
+                const string query = 
+                        @"SELECT TOP 6 TimeSheetID AS Id
+                            ,Company.CompanyName AS ClientName
+                            ,StatusID AS Status
+                            ,Periods.TimeSheetAvailablePeriodStartDate AS StartDate
+	                        ,Periods.TimeSheetAvailablePeriodEndDate AS EndDate
+                            ,User_Email.PrimaryEmail AS TimeSheetApprover
+                        FROM TimeSheet Times
+                        LEFT JOIN Agreement ON Agreement.AgreementID = Times.AgreementID
+                        LEFT JOIN Company ON Agreement.CompanyID = Company.CompanyID
+                        LEFT JOIN TimeSheetAvailablePeriod Periods ON Times.TimeSheetAvailablePeriodID = Periods.TimeSheetAvailablePeriodID
+                        LEFT JOIN User_Email ON Times.DirectReportUserId = User_Email.UserID
+                        WHERE CandidateUserID = @UserId
+                        ORDER BY EndDate DESC, CompanyName";
+                
+                var timesheets = db.Connection.Query<Timesheet>(query, new { UserId = userId});
 
-        public IEnumerable<Timesheet> GetTimesheetsForUser(int id)
-        {
-            return UserTimesheets;
+                return timesheets;
+            }
         }
 
         public Timesheet CreateTimesheet(Timesheet timesheet)
