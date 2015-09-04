96ffceb8a593eba92b96b0be9b190d9f0e7a1cf3
faraz.bhojani <faraz.bhojani@devfacto.com>
2015-09-04 11:22:09 -0600

    Added Pay Rates service/controller

---
+++ source/Web.Domain/Services/PayRateService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using SiSystems.Web.Domain.Context;
+using SiSystems.ConsultantApp.Web.Domain.Repositories;
+using SiSystems.SharedModels;
+
+namespace SiSystems.ConsultantApp.Web.Domain.Services
+{
+    public class PayRateService
+    {
+        private readonly ISessionContext _sessionContext;
+
+        public PayRateService(ISessionContext sessionContext)
+        {
+            _sessionContext = sessionContext;
+        }
+
+        public IEnumerable<string> GetPayRates()
+        {
+            List<String> list = new List<string>();
+            list.Add("Regular");
+            list.Add("Overtime");
+            list.Add("Special Rate");
+
+            IEnumerable<string> enumerableList = list.AsEnumerable();
+            return enumerableList;
+        }
+    }
+}
---
+++ source/Web/Controllers/Api/PayRatesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SiSystems.ConsultantApp.Web.Domain.Services;
+using SiSystems.ConsultantApp.Web.Filters;
+
+namespace SiSystems.ConsultantApp.Web.Controllers.Api
+{
+    [ConsultantAccessAuthorizationAttribute]
+    [RoutePrefix("api/PayRates")]
+    public class PayRatesController: ApiController
+    {
+        private readonly PayRateService _service;
+        public PayRatesController(PayRateService service)
+        {
+            _service = service;
+        }
+
+        public HttpResponseMessage GetPayRates()
+        {
+            var payRates = _service.GetPayRates();
+            return Request.CreateResponse(HttpStatusCode.OK, payRates);
+        }
+    }
+}
