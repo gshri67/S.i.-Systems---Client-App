61b5ac73adaddbfc7d8577eb086e1cabde4409ed
Anthony Sopkow <anthonysopkow@gmail.com>
2015-09-09 08:41:20 -0600

    Minor UI Changes

--- source/ConsultantApp/ConsultantApp.iOS/Remittances/RemittanceCell.designer.cs
+++ source/ConsultantApp/ConsultantApp.iOS/Remittances/RemittanceCell.designer.cs
@@ -1,14 +1,17 @@
 // WARNING
 //
-// This file has been generated automatically by Xamarin Studio to store outlets and
-// actions made in the UI designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
 using System.CodeDom.Compiler;
+using UIKit;
 
 namespace ConsultantApp.iOS
 {
+	[Register ("RemittanceCell")]
 	partial class RemittanceCell
 	{
 		[Outlet]
@@ -22,28 +25,9 @@ namespace ConsultantApp.iOS
 
 		[Outlet]
 		UIKit.UILabel periodLabel { get; set; }
-		
+
 		void ReleaseDesignerOutlets ()
 		{
-			if (amountLabel != null) {
-				amountLabel.Dispose ();
-				amountLabel = null;
-			}
-
-			if (depositDateLabel != null) {
-				depositDateLabel.Dispose ();
-				depositDateLabel = null;
-			}
-
-			if (periodLabel != null) {
-				periodLabel.Dispose ();
-				periodLabel = null;
-			}
-
-			if (documentNumberLabel != null) {
-				documentNumberLabel.Dispose ();
-				documentNumberLabel = null;
-			}
 		}
 	}
 }
