063ff5513db610649bc6b6d7d51cf65b95129111
Anthony Sopkow <anthonysopkow@gmail.com>
2015-11-06 15:55:27 -0700

    Adding Tests To  JobsClientListTableViewModel

---
+++ source/AccountExecutiveApp/AccountExecutiveApp.Core.Tests/TableViewSourceModels/JobsClientListTableViewModelTests.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountExecutiveApp.Core.TableViewSourceModel;
+using NUnit.Framework;
+using SiSystems.SharedModels;
+
+namespace AccountExecutiveApp.Core.Tests.TableViewSourceModels
+{
+    [TestFixture]
+    public class JobsClientListTableViewModelTests
+    {
+        private JobsClientListTableViewModel _viewModel; 
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        private static Job _jobCompanyOneDeveloperWithoutProposed = new Job
+        {
+            ClientName = "Company One",
+            JobTitle = "Developer",
+            hasCallout = false,
+            isProposed = false,
+            issueDate = DateTime.Now
+        };
+
+        private static Job _jobCompanyTwoDeveloperWithoutProposed = new Job
+        {
+            ClientName = "Company Two",
+            JobTitle = "Developer",
+            hasCallout = false,
+            isProposed = false,
+            issueDate = DateTime.Now
+        };
+
+        private static IEnumerable<Job> _jobsFromSameCompany = new List<Job>
+        {
+            _jobCompanyOneDeveloperWithoutProposed,
+            _jobCompanyOneDeveloperWithoutProposed,
+        }.AsEnumerable();
+
+        private static IEnumerable<Job> _jobsFromTwoCompanies = new List<Job>
+        {
+            _jobCompanyOneDeveloperWithoutProposed,
+            _jobCompanyTwoDeveloperWithoutProposed,
+        }.AsEnumerable();
+        
+        [Test]
+        public void NumberOfGroups_WhenJobsNull_ReturnsZero()
+        {
+            _viewModel = new JobsClientListTableViewModel(null);
+
+            var actual = _viewModel.NumberOfGroups();
+
+            Assert.AreEqual(0, actual);
+        }
+
+        [Test]
+        public void NumberOfGroups_MultipleJobsSameCompany_ReturnsOne()
+        {
+            _viewModel = new JobsClientListTableViewModel(_jobsFromSameCompany);
+            
+            var actual = _viewModel.NumberOfGroups();
+
+            Assert.AreEqual(1, actual);
+        }
+
+        [Test]
+        public void NumberOfGroups_MultipleJobsFromTwoCompanies_ReturnsTwo()
+        {
+            _viewModel = new JobsClientListTableViewModel(_jobsFromTwoCompanies);
+
+            var actual = _viewModel.NumberOfGroups();
+
+            Assert.AreEqual(2, actual);
+        }
+
+        [Test]
+        public void IndexIsInBounds_WithNullJobsAndAnyInput_ReturnsFalse()
+        {
+            _viewModel = new JobsClientListTableViewModel(null);
+
+            Assert.False(_viewModel.IndexIsInBounds(0));
+            Assert.False(_viewModel.IndexIsInBounds(-1));
+            Assert.False(_viewModel.IndexIsInBounds(1));
+        }
+
+        [Test]
+        public void IndexIsInBounds_TwoCompanies_ZeroAndOneReturnTrue()
+        {
+            _viewModel = new JobsClientListTableViewModel(_jobsFromTwoCompanies);
+
+            Assert.True(_viewModel.IndexIsInBounds(0));
+            Assert.True(_viewModel.IndexIsInBounds(1));
+        }
+
+        [Test]
+        public void IndexIsInBounds_TwoCompanies_NegativeOneAndTwoReturnFalse()
+        {
+            _viewModel = new JobsClientListTableViewModel(_jobsFromTwoCompanies);
+
+            Assert.False(_viewModel.IndexIsInBounds(-1));
+            Assert.False(_viewModel.IndexIsInBounds(2));
+        }
+
+        [Test]
+        public void ClientNameByRowNumber_WithNullJobs_ReturnsEmptyString()
+        {
+            _viewModel = new JobsClientListTableViewModel(null);
+            
+            var actual = _viewModel.ClientNameByRowNumber(0);
+
+            Assert.AreEqual(string.Empty, actual);
+        }
+
+        [Test]
+        public void JobsByRowNumber_WhenJobsNull_ReturnsEmptyEnumerable()
+        {
+            _viewModel = new JobsClientListTableViewModel(null);
+
+            var actual = _viewModel.JobsByRowNumber(0);
+
+            Assert.AreEqual(Enumerable.Empty<Job>(), actual);
+        }
+    }
+}
--- source/AccountExecutiveApp/AccountExecutiveApp.Core/TableViewSourceModel/JobsClientListTableViewModel.cs
+++ source/AccountExecutiveApp/AccountExecutiveApp.Core/TableViewSourceModel/JobsClientListTableViewModel.cs
@@ -9,51 +9,73 @@ namespace AccountExecutiveApp.Core.TableViewSourceModel
 {
     public class JobsClientListTableViewModel
     {
-        private readonly IEnumerable<IGrouping<string, Job>> _groupedJobs;
+        private IEnumerable<IGrouping<string, Job>> _groupedJobs;
+        private IEnumerable<IGrouping<string, Job>> GroupedJobs
+        {
+            get { return _groupedJobs ?? Enumerable.Empty<IGrouping<string, Job>>(); }
+            set { _groupedJobs = value ?? Enumerable.Empty<IGrouping<string, Job>>(); }
+        }
 
         public JobsClientListTableViewModel(IEnumerable<Job> jobs)
         {
+            GroupedJobs = jobs == null 
+                ? Enumerable.Empty<IGrouping<string, Job>>() 
+                : jobs.GroupBy(job => job.ClientName);
+        }
 
-            _groupedJobs = jobs.GroupBy(job => job.ClientName) ?? new List<IGrouping<string, Job>>().AsEnumerable();
+        public bool IndexIsInBounds(int index)
+        {
+            return index < GroupedJobs.Count() && index >= 0;
         }
 
         public int NumberOfGroups()
         {
-            return _groupedJobs.Count();
+            return GroupedJobs.Count();
         }
 
         public string ClientNameByRowNumber(int groupNumber)
         {
-            return _groupedJobs.ElementAtOrDefault(groupNumber).Key;
+            return IndexIsInBounds(groupNumber) 
+                ? GroupedJobs.ElementAtOrDefault(groupNumber).Key 
+                : string.Empty;
         }
 
         public int NumberOfJobsInSection(int groupNumber)
         {
-            return _groupedJobs.ElementAtOrDefault(groupNumber).Count();
+            return IndexIsInBounds(groupNumber)
+                ? GroupedJobs.ElementAtOrDefault(groupNumber).Count()
+                : 0;
         }
 
         public int NumberOfProposedJobsInSection(int groupNumber)
         {
-            return _groupedJobs.ElementAtOrDefault(groupNumber).Count(job => job.isProposed);
+            return IndexIsInBounds(groupNumber)
+                ? GroupedJobs.ElementAtOrDefault(groupNumber).Count(job => job.isProposed)
+                : 0;
         }
 
         public int NumberOfJobsWithCalloutsInSection(int groupNumber)
         {
-            return _groupedJobs.ElementAtOrDefault(groupNumber).Count(job => job.hasCallout);
+            return IndexIsInBounds(groupNumber) 
+                ? GroupedJobs.ElementAtOrDefault(groupNumber).Count(job => job.hasCallout)
+                : 0;
         }
 
         public string JobStateCountsByRowNumber(int groupNumber)
         {
-            var numJobs = NumberOfJobsInSection(groupNumber);
-            var jobsProposed = NumberOfProposedJobsInSection(groupNumber);
-            var jobCallouts = NumberOfJobsWithCalloutsInSection(groupNumber);
-
-            return string.Format("{0}/{1}/{2}", numJobs, jobsProposed, jobCallouts);
+            return IndexIsInBounds(groupNumber) 
+                ? string.Format("{0}/{1}/{2}", 
+                    NumberOfJobsInSection(groupNumber), 
+                    NumberOfProposedJobsInSection(groupNumber), 
+                    NumberOfJobsWithCalloutsInSection(groupNumber))
+                : string.Empty;
         }
 
         public IEnumerable<Job> JobsByRowNumber(int groupNumber)
         {
-            return _groupedJobs.ElementAtOrDefault(groupNumber);
+            return IndexIsInBounds(groupNumber)
+                ? GroupedJobs.ElementAtOrDefault(groupNumber)
+                : Enumerable.Empty<Job>();
         }
     }
 }
