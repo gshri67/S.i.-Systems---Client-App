36de85cd4079ed57f010d45c1891ec12825301fd
Anthony Sopkow <anthonysopkow@gmail.com>
2015-11-19 14:39:26 -0700

    Moving the Consultant Contract Summary BaseClass

--- source/SharedModels/ConsultantContract.cs
+++ source/SharedModels/ConsultantContract.cs
@@ -2,6 +2,26 @@
 
 namespace SiSystems.SharedModels
 {
+    public class ConsultantContractSummary
+    {
+        public int ContractId { get; set; }
+
+        public string ContractorName { get; set; }
+        public string ClientName { get; set; }
+        public string Title { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public MatchGuideConstants.AgreementSubTypes AgreementSubType { get; set; }
+
+        public bool IsFloThru { get { return AgreementSubType == MatchGuideConstants.AgreementSubTypes.FloThru; } }
+
+        public bool IsFullySourced { get { return AgreementSubType == MatchGuideConstants.AgreementSubTypes.Consultant; } }
+
+        public ContractStatusType StatusType { get; set; }
+    }
+
     public class ConsultantContract : ConsultantContractSummary
     {
         public int ConsultantId { get; set; }
