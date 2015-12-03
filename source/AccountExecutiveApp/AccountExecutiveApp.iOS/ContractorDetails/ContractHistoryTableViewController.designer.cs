bed8477f7aebac344daf8131bd128a3407b94148
FarazBhojani <faraz.bhojani@devfacto.com>
2015-12-03 13:21:07 -0700

    Fixed Storyboard bug with contract history

--- source/AccountExecutiveApp/AccountExecutiveApp.iOS/ContractorDetails/ContractHistoryTableViewController.designer.cs
+++ source/AccountExecutiveApp/AccountExecutiveApp.iOS/ContractorDetails/ContractHistoryTableViewController.designer.cs
@@ -1,17 +1,21 @@
+// WARNING
 //
-// This file has been generated automatically by Visual Studio to store outlets and
-// actions made in the Xcode designer. If it is removed, they will be lost.
-// Manual changes to this file may not be handled correctly.
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
 //
 using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
 
-namespace AccountExecutiveApp.iOS.ContractorDetails
+namespace AccountExecutiveApp.iOS
 {
-    [Register("ContractHistoryTableViewController")]
-    partial class ContractHistoryTableViewController
-    {
-        void ReleaseDesignerOutlets()
-        {
-        }
-    }
+	[Register ("ContractHistoryTableViewController")]
+	partial class ContractHistoryTableViewController
+	{
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
 }
