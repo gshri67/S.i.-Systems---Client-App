f3ef6c69b973d91a6ac65403c123bf57ca6aa107
Anthony Sopkow <anthonysopkow@gmail.com>
2015-12-17 16:40:15 -0700

    Pointing to Prod in Dev for UAT Testing

--- source/Shared.Core/Settings.cs
+++ source/Shared.Core/Settings.cs
@@ -8,10 +8,9 @@
         public const string MatchGuideApiAddress = "http://clientapi.local:50021/api/";
     #elif DEV
         //public const string MatchGuideApiAddress = "https://clientapidev.azurewebsites.net/api/";
-        public const string MatchGuideApiAddress = "https://clientapi.azurewebsites.net/api/";
     #elif TEST
         //public const string MatchGuideApiAddress = "https://clientapitest.azurewebsites.net/api/";
-        public const string MatchGuideApiAddress = "https://clientapidev.azurewebsites.net/api/";
+        public const string MatchGuideApiAddress = "https://clientapi.azurewebsites.net/api/";
     #elif PROD
         public const string MatchGuideApiAddress = "https://clientapi.azurewebsites.net/api/";
     #endif
