f3653e061e618a142bf51240e3cd16d81ded0a90
Anthony Sopkow <anthonysopkow@gmail.com>
2016-03-03 16:00:32 -0700

    Submit Zero Time submits with Correct Status

--- source/Web.Domain/Repositories/TimesheetRepository.cs
+++ source/Web.Domain/Repositories/TimesheetRepository.cs
@@ -322,7 +322,7 @@ namespace SiSystems.ConsultantApp.Web.Domain.Repositories
                           ,@TSstatus
                      SET @newTSID =  @@identity                  
                      SELECT @newTSID as TimesheetId";
-
+                var status = MatchGuideConstants.TimesheetStatus.Approved;
                 var savedTimesheetTempId = db.Connection.Query<int>(query, new
                 {
                     aTimesheetId = IntegerOrNullIfZero(timesheet.Id),
@@ -332,7 +332,7 @@ namespace SiSystems.ConsultantApp.Web.Domain.Repositories
                     aTSSubmittedName = (string)null, //Name of the Submitted PDF
                     verticalId = MatchGuideConstants.VerticalId.IT,
                     aTimesheetType = "ETimesheet",
-                    TSstatus = MatchGuideConstants.TimesheetStatus.Approved.ToString()
+                    TSstatus = status.ToString()
                 }).FirstOrDefault();
 
                 return savedTimesheetTempId;
