5936467119982aa9689da5f0cad4973761875c80
faraz.bhojani <faraz.bhojani@devfacto.com>
2016-03-10 10:54:54 -0700

    Centred CreateContractJobDescriptionView

--- source/AccountExecutiveApp/AccountExecutiveApp.iOS/ContractorDetails/CreateContractJobDescriptionView.cs
+++ source/AccountExecutiveApp/AccountExecutiveApp.iOS/ContractorDetails/CreateContractJobDescriptionView.cs
@@ -34,15 +34,19 @@ namespace AccountExecutiveApp.iOS
         {
             BackgroundColor = UIColor.FromWhiteAlpha(0.95f, 1.0f);
 
+            ContainerView = new UIView();
+            ContainerView.TranslatesAutoresizingMaskIntoConstraints = false;
+            AddSubview(ContainerView);
+
             TextView = new ContractCreationDetailsHeaderView(_description);
             TextView.TranslatesAutoresizingMaskIntoConstraints = false;
-            AddSubview(TextView);
+            ContainerView.AddSubview(TextView);
 
             CreateContractButton = new BorderedButton();
             CreateContractButton.SetTitle("Create Contract", UIControlState.Normal);
             CreateContractButton.SetTitleColor(StyleGuideConstants.RedUiColor, UIControlState.Normal);
             CreateContractButton.TranslatesAutoresizingMaskIntoConstraints = false;
-            AddSubview(CreateContractButton);
+            ContainerView.AddSubview(CreateContractButton);
 
             SetupConstraints();
         }
@@ -51,16 +55,26 @@ namespace AccountExecutiveApp.iOS
         {
             int separation = 8;
 
+            AddConstraint(NSLayoutConstraint.Create(ContainerView, NSLayoutAttribute.Left, NSLayoutRelation.Equal, this, NSLayoutAttribute.Left, 1.0f, 0f));
+            AddConstraint(NSLayoutConstraint.Create(ContainerView, NSLayoutAttribute.Right, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 1.0f, 0f));
+            AddConstraint(NSLayoutConstraint.Create(ContainerView, NSLayoutAttribute.CenterY, NSLayoutRelation.Equal, this, NSLayoutAttribute.CenterY, 1.0f, 0f));
+            
             //Add text description view to the top
-            AddConstraint(NSLayoutConstraint.Create(TextView, NSLayoutAttribute.Left, NSLayoutRelation.Equal, this, NSLayoutAttribute.Left, 1.0f, 0f));
-            AddConstraint(NSLayoutConstraint.Create(TextView, NSLayoutAttribute.Right, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 1.0f, 0f));
-            AddConstraint(NSLayoutConstraint.Create(TextView, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, this, NSLayoutAttribute.Bottom, 0.5f, -separation/2));
-            AddConstraint(NSLayoutConstraint.Create(TextView, NSLayoutAttribute.Top, NSLayoutRelation.GreaterThanOrEqual, this, NSLayoutAttribute.Top, 1.0f, 0f));
+            AddConstraint(NSLayoutConstraint.Create(TextView, NSLayoutAttribute.Left, NSLayoutRelation.Equal, ContainerView, NSLayoutAttribute.Left, 1.0f, 0f));
+            AddConstraint(NSLayoutConstraint.Create(TextView, NSLayoutAttribute.Right, NSLayoutRelation.Equal, ContainerView, NSLayoutAttribute.Right, 1.0f, 0f));
+            AddConstraint(NSLayoutConstraint.Create(TextView, NSLayoutAttribute.Top, NSLayoutRelation.Equal, ContainerView, NSLayoutAttribute.Top, 1.0f, 0f));
 
-            AddConstraint(NSLayoutConstraint.Create(CreateContractButton, NSLayoutAttribute.CenterX, NSLayoutRelation.Equal, this, NSLayoutAttribute.CenterX, 1.0f, 0f));
+            AddConstraint(NSLayoutConstraint.Create(CreateContractButton, NSLayoutAttribute.CenterX, NSLayoutRelation.Equal, ContainerView, NSLayoutAttribute.CenterX, 1.0f, 0f));
             AddConstraint(NSLayoutConstraint.Create(CreateContractButton, NSLayoutAttribute.Width, NSLayoutRelation.Equal, null, NSLayoutAttribute.NoAttribute, 1.0f, 175f));
             AddConstraint(NSLayoutConstraint.Create(CreateContractButton, NSLayoutAttribute.Top, NSLayoutRelation.Equal, TextView, NSLayoutAttribute.Bottom, 1.0f, separation/2));
-            AddConstraint(NSLayoutConstraint.Create(CreateContractButton, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, this, NSLayoutAttribute.Bottom, 1.0f, -12f));
+            AddConstraint(NSLayoutConstraint.Create(CreateContractButton, NSLayoutAttribute.Height, NSLayoutRelation.Equal, null, NSLayoutAttribute.NoAttribute, 1.0f, 45));
+
+
+            AddConstraint(NSLayoutConstraint.Create(ContainerView, NSLayoutAttribute.Top, NSLayoutRelation.Equal, TextView, NSLayoutAttribute.Top, 1.0f, 0f));
+            AddConstraint(NSLayoutConstraint.Create(ContainerView, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, CreateContractButton, NSLayoutAttribute.Bottom, 1.0f, 0f));
+
+            AddConstraint(NSLayoutConstraint.Create(ContainerView, NSLayoutAttribute.Top, NSLayoutRelation.GreaterThanOrEqual, this, NSLayoutAttribute.Top, 1.0f, 4f));
+            AddConstraint(NSLayoutConstraint.Create(ContainerView, NSLayoutAttribute.Bottom, NSLayoutRelation.LessThanOrEqual, this, NSLayoutAttribute.Bottom, 1.0f, -4f));
         }
 
     }
