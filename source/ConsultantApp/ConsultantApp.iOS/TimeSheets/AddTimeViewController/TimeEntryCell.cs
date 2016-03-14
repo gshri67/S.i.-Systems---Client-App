c5cde3cfe850a4a9ff493f43c8f70281bf4bd988
Anthony Sopkow <anthonysopkow@gmail.com>
2016-03-14 15:20:08 -0600

    Further Cleaning up TimeEntry Cell

--- source/ConsultantApp/ConsultantApp.iOS/TimeSheets/AddTimeViewController/TimeEntryCell.cs
+++ source/ConsultantApp/ConsultantApp.iOS/TimeSheets/AddTimeViewController/TimeEntryCell.cs
@@ -9,10 +9,9 @@ namespace ConsultantApp.iOS
     public class TimeEntryCell : UITableViewCell
     {
         private TimeEntry Entry { get; set; }
-		private UILabel clientField;
-        private UILabel projectCodeField;
-        private UITextField hoursField;
-		private UILabel payRateLabel;
+		private UILabel _projectCodeField;
+        private UITextField _hoursField;
+		private UILabel _payRateLabel;
 
         private float _maxNumberOfHours;
 
@@ -24,68 +23,77 @@ namespace ConsultantApp.iOS
         {
             TextLabel.Hidden = true;
 
-			clientField = new UILabel();
-			clientField.TranslatesAutoresizingMaskIntoConstraints = false;
-			AddSubview( clientField );
+			AddProjectCodeField();
 
-			projectCodeField = new UILabel();
-			projectCodeField.TranslatesAutoresizingMaskIntoConstraints = false;
-			projectCodeField.TextAlignment = UITextAlignment.Left;
-			projectCodeField.Font =  UIFont.FromName("Helvetica-Bold", 12f);
-			AddSubview( projectCodeField );
+            AddPayRateLabel();
 
-			payRateLabel = new UILabel();
-			payRateLabel.TranslatesAutoresizingMaskIntoConstraints = false;
-			payRateLabel.TextAlignment = UITextAlignment.Left;
-			payRateLabel.Font =  UIFont.FromName("Helvetica", 10f);
-			payRateLabel.TextColor = StyleGuideConstants.MediumGrayUiColor;
-			AddSubview( payRateLabel );
-
-			clientField.TextColor = UIColor.FromWhiteAlpha (0.55f, 1.0f);
-
-			clientField.Hidden = true;
-
-            hoursField = new UITextField();
-            hoursField.Text = "8";
-            hoursField.TranslatesAutoresizingMaskIntoConstraints = false;
-            hoursField.TextAlignment = UITextAlignment.Right;
-
-            hoursField.EditingChanged += delegate
-            {
-                if (!HoursAreValid())
-                    NotifyOfInvalidHoursAndSetToClosestValidHours();
-            };
-
-            hoursField.EditingDidEnd += delegate
-            {
-                SetHoursAndChangeEntry();
-            };
-
-			hoursField.EditingDidBegin += delegate {
-				this.BeginInvokeOnMainThread(SelectHoursTextFieldForEdit);
-			};
-
-			hoursField.KeyboardType = UIKeyboardType.DecimalPad;
-
-			var toolbar = new UIToolbar(new CoreGraphics.CGRect(0.0f, 0.0f, Frame.Size.Width, 44.0f));
-
-			toolbar.Items = new[]
-			{
-				new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
-				new UIBarButtonItem(UIBarButtonSystemItem.Done, doneButtonTapped)
-			};
-
-			hoursField.InputAccessoryView = toolbar;
-			hoursField.BorderStyle = UITextBorderStyle.RoundedRect;
-
-            AddSubview( hoursField );
+            AddHoursField();
 
             setupConstraints();
         }
 
+        private void AddHoursField()
+        {
+            var toolbar = new UIToolbar(new CoreGraphics.CGRect(0.0f, 0.0f, Frame.Size.Width, 44.0f))
+            {
+                Items = new[]
+                {
+                    new UIBarButtonItem(UIBarButtonSystemItem.FlexibleSpace),
+                    new UIBarButtonItem(UIBarButtonSystemItem.Done, doneButtonTapped)
+                }
+            };
+
+            _hoursField = new UITextField
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                TextAlignment = UITextAlignment.Right,
+                KeyboardType = UIKeyboardType.DecimalPad,
+                BorderStyle = UITextBorderStyle.RoundedRect,
+                InputAccessoryView = toolbar
+            };
+
+            AddHoursFieldDelegates();
+
+            AddSubview(_hoursField);
+        }
+
+        private void AddHoursFieldDelegates()
+        {
+            _hoursField.EditingChanged += delegate
+            {
+                if (!HoursAreValid())
+                    NotifyOfInvalidHoursAndSetToClosestValidHours();
+            };
+            _hoursField.EditingDidEnd += delegate { SetHoursAndChangeEntry(); };
+            _hoursField.EditingDidBegin += delegate { this.BeginInvokeOnMainThread(SelectHoursTextFieldForEdit); };
+        }
+
+        private void AddPayRateLabel()
+        {
+            _payRateLabel = new UILabel
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                TextAlignment = UITextAlignment.Left,
+                Font = UIFont.FromName("Helvetica", 10f),
+                TextColor = StyleGuideConstants.MediumGrayUiColor
+            };
+            AddSubview(_payRateLabel);
+        }
+
+        private void AddProjectCodeField()
+        {
+            _projectCodeField = new UILabel
+            {
+                TranslatesAutoresizingMaskIntoConstraints = false,
+                TextAlignment = UITextAlignment.Left,
+                Font = UIFont.FromName("Helvetica-Bold", 12f)
+            };
+            AddSubview(_projectCodeField);
+        }
+
         private void SelectHoursTextFieldForEdit()
         {
-            hoursField.SelectedTextRange = hoursField.GetTextRange(hoursField.BeginningOfDocument, hoursField.EndOfDocument);
+            _hoursField.SelectedTextRange = _hoursField.GetTextRange(_hoursField.BeginningOfDocument, _hoursField.EndOfDocument);
         }
 
         private void NotifyOfInvalidHoursAndSetToClosestValidHours()
@@ -109,13 +117,13 @@ namespace ConsultantApp.iOS
         private void ResetToMaximumHoursAndNotify()
         {
             ShowInvalidTimeAlert("Please enter less than 24 hours of total entries for the day.");
-            hoursField.Text = _maxNumberOfHours.ToString(CultureInfo.InvariantCulture);
+            _hoursField.Text = _maxNumberOfHours.ToString(CultureInfo.InvariantCulture);
         }
 
         private void ResetToMinimumHoursAndNotify()
         {
             ShowInvalidTimeAlert("Unable to enter negative time entries.");
-            hoursField.Text = 0.ToString();
+            _hoursField.Text = 0.ToString();
         }
 
         private void SetHoursAndChangeEntry()
@@ -152,13 +160,13 @@ namespace ConsultantApp.iOS
         private float HoursEnteredIfParsable()
         {
             float hours;
-            var parsed = float.TryParse(hoursField.Text, out hours);
+            var parsed = float.TryParse(_hoursField.Text, out hours);
             return parsed ? hours : 0;
         }
 
         public void doneButtonTapped(object sender, EventArgs args)
 		{
-			hoursField.ResignFirstResponder ();
+			_hoursField.ResignFirstResponder ();
 		}
 
 		//dismiss keyboard when tapping outside of text fields
@@ -166,39 +174,39 @@ namespace ConsultantApp.iOS
 		{
 			base.TouchesBegan(touches, evt);
 
-			hoursField.ResignFirstResponder();
+			_hoursField.ResignFirstResponder();
 		}
 
         public void setupConstraints() 
         {
-			AddConstraint( NSLayoutConstraint.Create(projectCodeField, NSLayoutAttribute.Left, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 0.05f, 0f));
-			AddConstraint(NSLayoutConstraint.Create(projectCodeField, NSLayoutAttribute.Top, NSLayoutRelation.Equal, this, NSLayoutAttribute.Bottom, 0.05f, 0f));
-			AddConstraint(NSLayoutConstraint.Create(projectCodeField, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, this, NSLayoutAttribute.Bottom, 0.65f, 0f));
-			AddConstraint(NSLayoutConstraint.Create(projectCodeField, NSLayoutAttribute.Right, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 0.75f, 0f));
+			AddConstraint( NSLayoutConstraint.Create(_projectCodeField, NSLayoutAttribute.Left, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 0.05f, 0f));
+			AddConstraint(NSLayoutConstraint.Create(_projectCodeField, NSLayoutAttribute.Top, NSLayoutRelation.Equal, this, NSLayoutAttribute.Bottom, 0.05f, 0f));
+			AddConstraint(NSLayoutConstraint.Create(_projectCodeField, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, this, NSLayoutAttribute.Bottom, 0.65f, 0f));
+			AddConstraint(NSLayoutConstraint.Create(_projectCodeField, NSLayoutAttribute.Right, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 0.75f, 0f));
 
-			AddConstraint( NSLayoutConstraint.Create(payRateLabel, NSLayoutAttribute.Left, NSLayoutRelation.Equal, projectCodeField, NSLayoutAttribute.Left, 1.0f, 0f));
-			AddConstraint(NSLayoutConstraint.Create(payRateLabel, NSLayoutAttribute.Top, NSLayoutRelation.Equal, projectCodeField, NSLayoutAttribute.Bottom, 1.0f, 0f));
-			AddConstraint(NSLayoutConstraint.Create(payRateLabel, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, this, NSLayoutAttribute.Bottom, 0.95f, 0f));
-			AddConstraint(NSLayoutConstraint.Create(payRateLabel, NSLayoutAttribute.Right, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 0.75f, 0f));
+			AddConstraint( NSLayoutConstraint.Create(_payRateLabel, NSLayoutAttribute.Left, NSLayoutRelation.Equal, _projectCodeField, NSLayoutAttribute.Left, 1.0f, 0f));
+			AddConstraint(NSLayoutConstraint.Create(_payRateLabel, NSLayoutAttribute.Top, NSLayoutRelation.Equal, _projectCodeField, NSLayoutAttribute.Bottom, 1.0f, 0f));
+			AddConstraint(NSLayoutConstraint.Create(_payRateLabel, NSLayoutAttribute.Bottom, NSLayoutRelation.Equal, this, NSLayoutAttribute.Bottom, 0.95f, 0f));
+			AddConstraint(NSLayoutConstraint.Create(_payRateLabel, NSLayoutAttribute.Right, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 0.75f, 0f));
 
-			AddConstraint(NSLayoutConstraint.Create(hoursField, NSLayoutAttribute.Left, NSLayoutRelation.Equal, payRateLabel, NSLayoutAttribute.Right, 1.0f, 0f));
-			AddConstraint(NSLayoutConstraint.Create(hoursField, NSLayoutAttribute.CenterY, NSLayoutRelation.Equal, this, NSLayoutAttribute.CenterY, 1.0f, 0f));
-			AddConstraint(NSLayoutConstraint.Create(hoursField, NSLayoutAttribute.Height, NSLayoutRelation.Equal, this, NSLayoutAttribute.Height, 0.6f, 0f));
-            AddConstraint(NSLayoutConstraint.Create(hoursField, NSLayoutAttribute.Right, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 0.95f, 0f));
+			AddConstraint(NSLayoutConstraint.Create(_hoursField, NSLayoutAttribute.Left, NSLayoutRelation.Equal, _payRateLabel, NSLayoutAttribute.Right, 1.0f, 0f));
+			AddConstraint(NSLayoutConstraint.Create(_hoursField, NSLayoutAttribute.CenterY, NSLayoutRelation.Equal, this, NSLayoutAttribute.CenterY, 1.0f, 0f));
+			AddConstraint(NSLayoutConstraint.Create(_hoursField, NSLayoutAttribute.Height, NSLayoutRelation.Equal, this, NSLayoutAttribute.Height, 0.6f, 0f));
+            AddConstraint(NSLayoutConstraint.Create(_hoursField, NSLayoutAttribute.Right, NSLayoutRelation.Equal, this, NSLayoutAttribute.Right, 0.95f, 0f));
         }
 
 		public void enable(bool shouldEnable )
 		{
 			if (shouldEnable) 
 			{
-				hoursField.BorderStyle = UITextBorderStyle.RoundedRect;
-				hoursField.Enabled = true;
-				hoursField.TextColor = UIColor.Black;
+				_hoursField.BorderStyle = UITextBorderStyle.RoundedRect;
+				_hoursField.Enabled = true;
+				_hoursField.TextColor = UIColor.Black;
 			} else 
 			{
-				hoursField.BorderStyle = UITextBorderStyle.None;
-				hoursField.Enabled = false;
-				hoursField.TextColor = UIColor.LightGray;
+				_hoursField.BorderStyle = UITextBorderStyle.None;
+				_hoursField.Enabled = false;
+				_hoursField.TextColor = UIColor.LightGray;
 			}
 		}
 
@@ -206,15 +214,15 @@ namespace ConsultantApp.iOS
         {
             Entry = timeEntry;
 
-            projectCodeField.Text = Entry.CodeRate.PONumber;
-            payRateLabel.Text = Entry.CodeRate.ratedescription;
-            hoursField.Text = string.Format("{0}", Entry.Hours);
+            _projectCodeField.Text = Entry.CodeRate.PONumber;
+            _payRateLabel.Text = Entry.CodeRate.ratedescription;
+            _hoursField.Text = string.Format("{0}", Entry.Hours);
             _maxNumberOfHours = maxNumberOfHours;
         }
 
         public void FocusOnHours()
         {
-            hoursField.BecomeFirstResponder();
+            _hoursField.BecomeFirstResponder();
         }
     }
 }
