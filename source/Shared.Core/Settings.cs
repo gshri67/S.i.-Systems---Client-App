7235e18b8b54b196e539ced8d699f1301bdbbe14
Anthony Sopkow <anthonysopkow@gmail.com>
2016-03-30 11:15:56 -0600

    Correcting the Settings File to point Test and Test API

--- source/Shared.Core/Settings.cs
+++ source/Shared.Core/Settings.cs
@@ -3,15 +3,13 @@
     public static class Settings
     {
         public const string DemoDomain = "@siAKSdemo.com";
-        public const string DemoMatchGuideApiAddress = "https://clientapitest.azurewebsites.net/api/";
+        public const string DemoMatchGuideApiAddress = "https://clientapitest.azurewebsites.net/api/";//todo:change to Demo Api When available.
     #if LOCAL
         public const string MatchGuideApiAddress = "http://clientapi.local:50021/api/";
     #elif DEV
         public const string MatchGuideApiAddress = "https://clientapidev.azurewebsites.net/api/";
     #elif TEST
-        public const string MatchGuideApiAddress = "https://clientapidev.azurewebsites.net/api/";
-        //public const string MatchGuideApiAddress = "https://clientapitest.azurewebsites.net/api/";
-        //public const string MatchGuideApiAddress = "https://clientapi.azurewebsites.net/api/";
+        public const string MatchGuideApiAddress = "https://clientapitest.azurewebsites.net/api/";
     #elif PROD
         public const string MatchGuideApiAddress = "https://clientapi.azurewebsites.net/api/";
 #endif
