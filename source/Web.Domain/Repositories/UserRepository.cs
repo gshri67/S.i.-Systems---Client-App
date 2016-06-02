f73476b27a13bb9614bdbfe097219e96124b1702
faraz.bhojani <faraz.bhojani@devfacto.com>
2016-06-02 11:51:55 -0600

    Formatting new login SQL

--- source/Web.Domain/Repositories/UserRepository.cs
+++ source/Web.Domain/Repositories/UserRepository.cs
@@ -64,7 +64,25 @@ namespace SiSystems.ClientApp.Web.Domain.Repositories
         {
             using (var db = new DatabaseContext(DatabaseSelect.MatchGuide))
             {
-                const string query = "SELECT User_Login.UserId, users.[FirstName] FirstName, users.[LastName] LastName, users.[UserType], users.[ClientPortalTypeID] ClientPortalType, users.ClientPortalFTAlumniTypeID FloThruAlumniAccess, User_Login.[Login], User_Login.[Password] PasswordHash, C.[CompanyID] CompanyId, C.[CompanyName], C.[MaxVisibleRatePerHour] as ClientsMaxVisibleRate, C.[IsHavingFTAlumni] IsCompanyParticipating From     user_email INNER JOIN users ON users.userid = user_email.userid INNER JOIN [User_Login] ON [User_Login].userid = users.userid LEFT JOIN [Company] as C on users.CompanyID = C.CompanyID Where user_email.[PrimaryEmail] = @Username and User_Login.ForceUpdate = 0 and users.usertype in (select picklistid from dbo.udf_getpicklistids('userroles','Candidate,New Candidate',-1))";
+                const string query = @"SELECT User_Login.UserId, 
+                                        users.[FirstName] FirstName, 
+                                        users.[LastName] LastName, 
+                                        users.[UserType], 
+                                        users.[ClientPortalTypeID] ClientPortalType,
+                                        users.ClientPortalFTAlumniTypeID FloThruAlumniAccess,
+                                        User_Login.[Login],
+                                        User_Login.[Password] PasswordHash,
+                                        C.[CompanyID] CompanyId,
+                                        C.[CompanyName],
+                                        C.[MaxVisibleRatePerHour] as ClientsMaxVisibleRate,
+                                        C.[IsHavingFTAlumni] IsCompanyParticipating 
+                                        From     user_email 
+                                        INNER JOIN users ON users.userid = user_email.userid 
+                                        INNER JOIN [User_Login] ON [User_Login].userid = users.userid 
+                                        LEFT JOIN [Company] as C on users.CompanyID = C.CompanyID 
+                                        Where user_email.[PrimaryEmail] = @Username 
+                                        and User_Login.ForceUpdate = 0 
+                                        and users.usertype in (select picklistid from dbo.udf_getpicklistids('userroles','Candidate,New Candidate',-1))";
 
                 var user = db.Connection.Query<User>(query, new {Username = username}).FirstOrDefault();
                 SetAccessLevel(user);
